fcfc94c20d396999602e6d9c0b157a2db0769fc7
heykkyu <heykkyu@gmail.com>
2021-10-26 17:52:38 +0900

    typescript reducer add

---
+++ src/state/reducers/bankReducer.ts
@@ -0,0 +1,32 @@
+const initialSate = 0;
+
+interface DepositAction {
+  type: "deposit"
+  payload: number
+}
+
+interface WithdrawAction {
+  type: "withdraw"
+  payload: number
+}
+
+interface BankruptAction {
+  type: "bankrupt"
+}
+
+type Action = DepositAction | WithdrawAction | BankruptAction
+
+const reducer = (state: number = initialSate, action: Action) => {
+  switch (action.type) {
+    case "deposit":
+      return state + action.payload;
+    case "withdraw":
+      return state - action.payload;
+    case "bankrupt":
+      return 0;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
