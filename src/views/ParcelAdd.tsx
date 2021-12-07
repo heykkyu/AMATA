4a5b6b7d2249599d47f29cd75690fbc86f7b736b
heykkyu <heykkyu@gmail.com>
2021-12-07 15:31:22 +0900

    add button and textfiled added

--- src/views/ParcelAdd.tsx
+++ src/views/ParcelAdd.tsx
@@ -1,7 +1,21 @@
-import styled from 'styled-components'
 import { useDispatch, useSelector } from "react-redux"
 import { bindActionCreators } from 'redux';
 import { actionCreators, State } from '@src/state';
+import Lottie from 'react-lottie-player'
+import meditationlottie from '../assets/lottie/meditation.json'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components'
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
 const ParcelListWrap = styled.div`
 
@@ -12,10 +26,21 @@ const ParcelList = () => {
   const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(actionCreators, dispatch);
   const amount = useSelector((state: State) => state.bank)
 
+  const classes = useStyles();
+
   return (
     <>
       <ParcelListWrap>
-        추가
+        <Lottie
+          loop
+          animationData={meditationlottie}
+          style={{ height: 300 }}
+          play
+        />
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="outlined-basic" label="Tracking No" variant="outlined" />
+        </form>
+        <Button variant="contained" color="primary">조회</Button>
         <h1>{amount}</h1>
           <button onClick={() => depositMoney(1000)}>Deposit</button>
           <button onClick={() => withdrawMoney(500)}>Withdarw</button>
