308bf95ea786b45e14c1ff14ada87c965ab58a92
heykkyu <heykkyu@gmail.com>
2022-03-03 12:55:51 +0900

    tracking Add - carrier added

---
+++ src/components/parcel/TrackingInput.tsx
@@ -0,0 +1,124 @@
+import React, { useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useNavigate } from 'react-router';
+import styled from 'styled-components'
+import { addTracking } from '@src/modules/trackinglist'
+
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+      display: 'block'
+    },
+    marginBottom: '20px',
+  },
+  button: {
+    fontWeight: 'bold',
+    width: '27.5ch',
+    padding: '20px 0',
+  },
+}));
+
+const TrackingInput = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const classes = useStyles();
+
+  interface trackingType {
+    carrier: string,
+    tracking_no: string
+  }
+  const [trackingInfo, setTrackingInfo] = useState<trackingType>({
+    carrier: "",
+    tracking_no: "" 
+  });
+
+  const handleCarrier = (e:React.ChangeEvent<{ value: string }>)  => {
+    console.log('e', e)
+    setTrackingInfo(prev => ({
+      ...prev,
+      carrier: e.target.value
+    }))
+  }
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const regex = e.target.value.replace(/[^0-9]/g, "")
+    setTrackingInfo(prev => ({
+      ...prev,
+      tracking_no: String(regex)
+    }))
+  }
+
+  const addNewParcel = () => {
+    if (!trackingInfo.tracking_no || trackingInfo.tracking_no.length < 8) {
+      alert('Unvalide Tracking')
+    } else {
+      dispatch(addTracking(trackingInfo.carrier, trackingInfo.tracking_no));
+      navigate(`/detail/${trackingInfo.tracking_no}`)
+    }
+  }
+
+  const addNewEnter = (e:React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      addNewParcel();
+      e.preventDefault();
+    }
+  }
+  
+
+  return (
+    <>
+      <form className={classes.root} noValidate autoComplete="off">
+        <FormControl sx={{ m: 1, minWidth: 80 }} >
+          <InputLabel id="select-carrier-label">Carrier</InputLabel>
+            <Select
+              labelId="select-carrier-label"
+              id="select-carrier-label-select"
+              value={trackingInfo.carrier}
+              onChange={(e) => setTrackingInfo(prev => ({
+                ...prev,
+                carrier: e.target.value as string
+              }))}
+              label="Carrier"
+            >
+              <MenuItem disabled value="">
+                {/* <em> select</em> */}
+                선택
+              </MenuItem>
+              <MenuItem value='cj'>CJ Delivery</MenuItem>
+              <MenuItem value='usps'>USPS</MenuItem>
+              <MenuItem value='amazon'>Amazon Delivery</MenuItem>
+            </Select>
+          </FormControl>
+          <br/>
+            <TextField 
+            onChange={handleChange}
+            id="outlined-basic" 
+            variant="outlined"
+            label="type tracking number"
+            value={trackingInfo.tracking_no}
+            onKeyPress={addNewEnter}
+          />
+
+        </form>
+          <Button
+            variant="contained" 
+            color="primary"
+            onClick={addNewParcel}
+            className={classes.button}
+          >등록하기</Button>
+    </>
+  )
+
+}
+
+export default TrackingInput;
--- src/container/ParcelAdd.tsx
+++ src/container/ParcelAdd.tsx
@@ -2,98 +2,21 @@ import React, { useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import Lottie from 'react-lottie-player'
 import meditationlottie from '../assets/lottie/meditation.json'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import styled from 'styled-components'
-import { useNavigate } from 'react-router';
-import { addTracking } from '@src/modules/trackinglist'
+import TrackingInput from "@src/components/parcel/TrackingInput";
   
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-    },
-  }));
-
-const ParcelListWrap = styled.div`
-
-`
 
 const ParcelList = () => {
-  const dispatch = useDispatch();
-  const classes = useStyles();
-  const navigate = useNavigate();
-  // const onAdd = useCallback(() => dispatch(addTracking()), [dispatch]);
-
-  interface trackingType {
-    carrier: string,
-    tracking_no: string
-  }
-  const [trackingInfo, setTrackingInfo] = useState<trackingType>({
-    carrier: "",
-    tracking_no: "" 
-  });
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    const regex = e.target.value.replace(/[^0-9]/g, "")
-    setTrackingInfo(prev => ({
-      ...prev,
-      tracking_no: String(regex)
-    }))
-  }
-
-  const addNewParcel = () => {
-    if (!trackingInfo.tracking_no || trackingInfo.tracking_no.length < 8) {
-      alert('Unvalide Tracking')
-    } else {
-      dispatch(addTracking(trackingInfo.carrier, trackingInfo.tracking_no));
-      navigate(`/detail/${trackingInfo.tracking_no}`)
-    }
-  }
-
-  const addNewEnter = (e:React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      addNewParcel();
-      e.preventDefault();
-    }
-  }
-  
   return (
     <>
-    {/* 스타일 관련 태그들 components로 빼기  */}
-      <ParcelListWrap>
-        <Lottie
-          loop
-          animationData={meditationlottie}
-          style={{ height: 300 }}
-          play
-        />
-        <form className={classes.root} noValidate autoComplete="off">
-        <TextField 
-            onChange={handleChange}
-            id="outlined-basic" 
-            label="Carrier" 
-            variant="outlined"
-            value={trackingInfo.carrier}
-            onKeyPress={addNewEnter}
-          />
-          <TextField 
-            onChange={handleChange}
-            id="outlined-basic" 
-            label="Tracking No" 
-            variant="outlined"
-            value={trackingInfo.tracking_no}
-            onKeyPress={addNewEnter}
-          />
-        </form>
-        <Button
-          variant="contained" 
-          color="primary"
-          onClick={addNewParcel}
-        >등록하기</Button>
-      </ParcelListWrap>
+      <Lottie
+        loop
+        animationData={meditationlottie}
+        style={{ height: 300 }}
+        play
+      />
+      
+      <TrackingInput
+      />
     </>
   );
 }
